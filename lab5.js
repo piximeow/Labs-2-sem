17b0856994ee303c2890795a4f3a7928b06fd098
piximeow <annapolishchuk2015@gmail.com>
2026-04-21 21:43:55 +0300

    add demo usage for callback and promise versions, AbortController

--- lab5.js
+++ lab5.js
@@ -1,4 +1,8 @@
-export function asyncMapCallback(array, asyncFn, callback, options = {}) {
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function asyncMapCallback(array, asyncFn, callback, options = {}) {
     const results = [];
     let index = 0;
     const signal = options.signal;
@@ -72,3 +76,68 @@ function asyncMapPromise(array, asyncFn, options = {}) {
         next();
     });
 }
+
+async function runAsyncAwaitDemo() {
+    const data = [1, 2, 3, 4];
+
+    const result = await asyncMapPromise(data, async (num) => {
+        await delay(400);
+        return num * 2;
+    });
+
+    console.log("Async/Await result:", result);
+}
+
+function demoCallback() {
+    const controller = new AbortController();
+
+    asyncMapCallback(
+        [1, 2, 3],
+        (num, i, arr, cb) => {
+            setTimeout(() => {
+                cb(null, num * 10);
+            }, 300);
+        },
+        (err, result) => {
+            if (err) return console.error("Callback error:", err.message);
+            console.log("Callback result:", result);
+        },
+        { signal: controller.signal }
+    );
+}
+
+function demoPromise() {
+    const controller = new AbortController();
+
+    asyncMapPromise(
+        [5, 6, 7],
+        async (num) => {
+            await delay(300);
+            return num + 1;
+        },
+        { signal: controller.signal }
+    )
+        .then(result => console.log("Promise result:", result))
+        .catch(err => console.error("Promise error:", err.message));
+}
+
+function demoAbort() {
+    const controller = new AbortController();
+
+    asyncMapPromise(
+        [1, 2, 3, 4, 5],
+        async (num) => {
+            await delay(1000);
+            return num * 2;
+        },
+        { signal: controller.signal }
+    )
+        .then(result => console.log("Result:", result))
+        .catch(err => console.log("Aborted:", err.message));
+
+    
+    setTimeout(() => {
+        controller.abort();
+    }, 2000);
+}
+
