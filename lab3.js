488cf43ebb721e7fe565aea34ce844e04376f19d
piximeow <annapolishchuk2015@gmail.com>
2026-04-29 15:45:01 +0300

    add cache size limit support

--- lab3.js
+++ lab3.js
@@ -14,3 +14,30 @@ function memoize(fn) {
     return result;
   };
 }
+
+function memoize(fn, options = {}) {
+  const { maxSize = Infinity } = options;
+  const cache = new Map();
+
+  function evictIfNeeded() {
+    if (cache.size >= maxSize) {
+      const firstKey = cache.keys().next().value;
+      cache.delete(firstKey);
+    }
+  }
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn(...args);
+
+    evictIfNeeded();
+    cache.set(key, result);
+
+    return result;
+  };
+}
