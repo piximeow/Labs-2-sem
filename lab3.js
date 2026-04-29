bc0481793b7573df1f48416e4a68ebf94fb30a06
piximeow <annapolishchuk2015@gmail.com>
2026-04-29 15:45:52 +0300

    unify cache entry structure

--- lab3.js
+++ lab3.js
@@ -41,3 +41,35 @@ function memoize(fn, options = {}) {
     return result;
   };
 }
+
+function memoize(fn, options = {}) {
+  const { maxSize = Infinity } = options;
+  const cache = new Map();
+
+  function evictIfNeeded() {
+    if (cache.size >= maxSize) {
+      const firstKey = cache.keys().next().value;
+      cache.delete(firstKey);
+    }
+  }
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key).value;
+    }
+
+    const result = fn(...args);
+
+    evictIfNeeded();
+
+    cache.set(key, {
+      value: result,
+      time: Date.now(),
+      freq: 1
+    });
+
+    return result;
+  };
+}
