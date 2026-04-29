4a09d2753585fad128f23d71df7f63d7378b21b4
piximeow <annapolishchuk2015@gmail.com>
2026-04-29 15:49:16 +0300

    implement LRU (Least Recently Used) eviction policy

--- lab3.js
+++ lab3.js
@@ -73,3 +73,43 @@ function memoize(fn, options = {}) {
     return result;
   };
 }
+
+function memoize(fn, options = {}) {
+  const { maxSize = Infinity, policy = "lru" } = options;
+  const cache = new Map();
+
+  function evict() {
+    const firstKey = cache.keys().next().value;
+    cache.delete(firstKey);
+  }
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      const entry = cache.get(key);
+
+      entry.freq++;
+      entry.time = Date.now();
+
+      if (policy === "lru") {
+        cache.delete(key);
+        cache.set(key, entry);
+      }
+
+      return entry.value;
+    }
+
+    if (cache.size >= maxSize) evict();
+
+    const result = fn(...args);
+
+    cache.set(key, {
+      value: result,
+      time: Date.now(),
+      freq: 1
+    });
+
+    return result;
+  };
+}
